99c89c7b637f8a3a61261362e8c02b9daa3b16f8
dohaisalive <dohaisalive@gmail.com>
2022-07-02 01:24:06 +0300

    users and nav

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { observer } from "mobx-react-lite";
+import Main from "./screens/MainScreen";
+import ImageBrowser from "./screens/ImageBrowserScreen";
+import { StyleSheet } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import { configure } from "mobx";
+import SignUpStackNavigator from "./components/navigation/stackNavs/signUpStackNav";
 
-export default function App() {
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <SignUpStackNavigator />
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
